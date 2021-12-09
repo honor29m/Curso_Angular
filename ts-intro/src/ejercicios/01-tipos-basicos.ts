eae7d99fa4b89befcc20c946d826ebe83a0d545b
Honorio Rodriguez <honor29m@gmail.com>
2021-12-08 21:35:34 -0400

    01-tipos-basicos

---
+++ ts-intro/src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Honi';
+let hp: number | string = 95;
+let estaVivo: boolean = true;
+
+hp = 'Full';
+
+console.log(nombre, hp)
