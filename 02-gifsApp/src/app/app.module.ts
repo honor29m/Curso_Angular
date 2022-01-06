f8f119511a8579842654a2e62a172675f687afb2
Honorio Rodriguez <hrodriguez@payall.com.ve>
2022-01-06 15:00:55 -0400

    Modulo shared

--- 02-gifsApp/src/app/app.module.ts
+++ 02-gifsApp/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
