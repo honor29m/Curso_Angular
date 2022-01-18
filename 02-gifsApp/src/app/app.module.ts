81356d4bec6c59ff31a099554a71c946d9931a1e
Honorio Rodriguez <honor29m@gmail.com>
2022-01-17 20:52:55 -0400

    GifsModule y sus componentes

--- 02-gifsApp/src/app/app.module.ts
+++ 02-gifsApp/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
