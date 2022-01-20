dd00e565cbb52225c4511690183e496068cbbe6d
Honorio Rodriguez <honor29m@gmail.com>
2022-01-20 15:16:29 -0400

    GifsService

--- 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BusquedaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+
+  constructor(
+    private gifsService: GifsService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    this.gifsService.buscarGifs( valor );
+    this.txtBuscar.nativeElement.value = '';
   }
 
 }
